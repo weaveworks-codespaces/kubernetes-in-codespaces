d91a2d5da848383f7a26bb2a83f708658d31cdde
Tiffany Wang <5248989+tiffanywang3@users.noreply.github.com>
2022-03-29 11:40:49 +0100

    Add flux installation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,5 +9,13 @@ echo "$(date)    post-create start" >> "$HOME/status"
 git -C /workspaces/imdb-app pull
 git -C /workspaces/webvalidate pull
 
+flux install >> ~/status
+flux create source git "${organization,,}-${repository,,}" \
+    --url https://github.com/${organization}/${repository} \
+    --branch tw-test \
+    --username=PersonalAccessToken \
+    --password=${GITHUB_TOKEN} >> ~/status
+flux create kustomization "${organization,,}-${repository,,}" --source GitRepository/"${organization,,}-${repository,,}" --path="${deploymentPath}" --prune=true --interval=1m >> ~/status
+
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
